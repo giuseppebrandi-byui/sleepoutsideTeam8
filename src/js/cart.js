7c5e06dbfeff28da8e740fb8e729db8817143f6b
kateaeryn <kate.ae.christensen@gmail.com>
2023-09-26 15:19:10 -0700

    forgot to delete something

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,14 +2,13 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   let cartItems = getLocalStorage("so-cart");
-  
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
 function cartItemTemplate(item) {
-  
-    const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
         src="${item.Image}"
@@ -24,8 +23,7 @@ function cartItemTemplate(item) {
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
 
-    return newItem;
-  
+  return newItem;
 }
 function cartCounter() {
   let cart = getLocalStorage("so-cart");
