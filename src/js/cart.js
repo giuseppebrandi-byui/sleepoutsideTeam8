2b6e5e376685a5b1cb51893d952d7ac524e38f7a
kateaeryn <kate.ae.christensen@gmail.com>
2023-09-28 21:26:12 -0700

    got 2 out of 4 steps completed

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,14 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
-
 function renderCartContents() {
   let cartItems = getLocalStorage("so-cart");
+  cartItems = cartItems ? getLocalStorage("so-cart") : [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document
+    .getElementById("cart-card__remover")
+    .addEventListener("click", removeProduct());
 }
-
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
@@ -20,14 +22,22 @@ function cartItemTemplate(item) {
       <h2 class="card__name">${item.Name}</h2>
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <span class="cart-card__remover" id="${item.Id}">&#10006</span>
+    <span class="cart-card__remover" data-id="${item.Id}"><a href="#" >&#10006</a></span>
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
-  
+
   return newItem;
-  
 }
+
+// function updateCart() {
+//   let cart = getLocalStorage("so-cart");
+//   cart = cart ? getLocalStorage("so-cart") : [];
+//   let count = cart.length;
+//   if (count !== 0) {
+//     removeProduct();
+//   }
+// }
 
 function cartCounter() {
   let cart = getLocalStorage("so-cart");
@@ -38,14 +48,11 @@ function cartCounter() {
 renderCartContents();
 cartCounter();
 
-
-
 function removeProduct(item) {
   let cart = getLocalStorage("so-cart");
   let product = cart.indexOf(item);
-  if (cart == item) {
-    cart.splice(product, 1);
+  if (product !== -1) {
+    cart.splice(item);
   }
   setLocalStorage("so-cart", cart);
-  renderCartContents();
 }
