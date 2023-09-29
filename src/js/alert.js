ea3b068338a9b9ae07cddde929d764c18f6a5461
giuseppebrandi-byui <bra22041@byui.edu>
2023-09-29 07:46:56 +0100

    Implemented alert for index page

---
+++ src/js/alert.js
@@ -0,0 +1,16 @@
+const main = document.querySelector("main");
+const section = document.createElement("section");
+section.className = "alert-list";
+main.prepend(section);
+
+fetch("../alert.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // console.log(data);
+    data.forEach((post) => {
+      section.insertAdjacentHTML(
+        "beforeend",
+        `<p style="background:${post.background}; color:${post.color}; padding:.5rem">${post.message}</p>`
+      );
+    });
+  });
