7ac08f72e91c25593c7e5af81c755cbaade48206
giuseppebrandi-byui <bra22041@byui.edu>
2023-10-10 22:33:42 +0100

    run build

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
 import { getParam, loadHeaderFooter } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
+loadHeaderFooter();
 const productId = getParam("product");
 productDetails(productId);
-loadHeaderFooter();
