7bd3ac37687ee33a3200e70c4daa34ac45ac3827
giuseppebrandi-byui <bra22041@byui.edu>
2023-10-14 21:57:50 +0100

    Added sort functionality

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -17,33 +17,24 @@ export default async function productList(selector, category) {
   const el = document.querySelector(selector);
   const products = await getData(category);
   const selectMenu = document.querySelector("#sort-by");
-  const selectEl = document.querySelector('select[name="sort-by"]');
-  let selectValue = selectEl.value;
   let selectedProducts = products.filter(
     (product) => product.Id !== "989CG" && product.Id !== "880RT"
   );
-  // selectMenu.addEventListener("change", (event) => {
-  //   if (selectValue === "name") {
-  //     console.log("This is a name");
-  //   }
-  // });
-  if (selectValue === "name") {
-    selectedProducts.sort(
-      (a, b) => (a.NameWithoutBrand > b.NameWithoutBrand) - 1
-    );
-  } else if (selectValue === "price") {
-    selectedProducts.sort(
-      (price1, price2) => price1.FinalPrice - price2.FinalPrice
-    );
-  }
   renderListWithTemplate(productCardTemplate, el, selectedProducts);
+
+  selectMenu.addEventListener("change", () => {
+    const selectEl = document.querySelector('select[name="sort-by"]');
+    let selectValue = selectEl.value;
+    console.log(selectValue);
+    if (selectValue === "name") {
+      selectedProducts.sort(
+        (a, b) => (a.NameWithoutBrand > b.NameWithoutBrand) - 1
+      );
+    } else if (selectValue === "price") {
+      selectedProducts.sort(
+        (price1, price2) => price1.FinalPrice - price2.FinalPrice
+      );
+    }
+    renderListWithTemplate(productCardTemplate, el, selectedProducts);
+  });
 }
-
-//  const selectElement = document.querySelector(".Colours");
-//  selectElement.addEventListener("change", (event) => {
-//    const result = document.querySelector(".tutorial");
-//    result.textContent = `You like ${event.target.value}`;
-//  });
-
-// .sort((price1, price2) => price1.FinalPrice - price2.FinalPrice)
-// .sort((a, b) => (a.NameWithoutBrand > b.NameWithoutBrand) - 1)
