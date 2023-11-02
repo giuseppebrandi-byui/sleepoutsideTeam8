3506aa97660e1ba3174bb40214a5ca9387306e60
g-e-rusk <grusk92@gmail.com>
2023-11-01 18:14:00 -0700

    working on modal box for newsletter

--- src/js/newsletter.js
+++ src/js/newsletter.js
@@ -3,25 +3,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 // const form = document.querySelector("newsletter-form");
-const button = document.querySelector(".newsletter-button");
+const form = document.querySelector(".newsletter-form");
+const modal = document.getElementById("thank-you-modal");
 
 // This is to show the modal box on form submit
-button.addEventListener("submit", e => {
+form.addEventListener("submit", e => {
     e.preventDefault();
     showModal();
 });
 
-function showModal() {
-
-    const modal = document.getElementById("thank-you-modal");
-
+function showModal() {  
     modal.style.display = "block";
 }
 
 
 // This is to hide the modal box when you click it
-// modal.addEventListener("click", () => {
-//     modal.style.display = "none";
-// });
+modal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
 
 });
